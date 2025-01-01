a04f1a13ca111e9cdab1318bae8fbeb9cdd0b388
Husnulmab <Haiderhusnulmab60@gmail.com>
2025-01-02 03:23:52 +0500

    First Commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            "Introducing Forever, your ultimate destination for stylish and comfortable clothing. Our brand is built on the foundation of quality, creativity, and a passion for fashion. From statement-making dresses to everyday essentials, our collection is designed to make you feel confident, expressive, and uniquely you. Join the Forever movement and experience the art of effortless style."
+
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+92-212-456-7890</li>
+                <li>contact@admin.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ husnulmab - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
